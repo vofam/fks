0d012e6cd200b6323d221d62625c32deaeea0f11
Miek Gieben <miek@miek.nl>
2012-08-30 19:31:21 +0200

    add profiling and gofmt

--- fksd/main.go
+++ fksd/main.go
@@ -1,30 +1,40 @@
 package main
 
 import (
-	"github.com/miekg/dns"
 	"flag"
+	"github.com/miekg/dns"
 	"log"
 	"os"
 	"os/signal"
+	"runtime/pprof"
 	"strconv"
 	"strings"
 )
 
 var (
 	flaglog   = flag.Bool("log", false, "log incoming queries")
-	port = flag.Int("port", 1053, "port to use")
+	flagprof  = flag.String("prof", "", "write cpu profile to file")
+	port      = flag.Int("port", 1053, "port to use")
 	superuser = flag.String("user", "root", "username to use for the superuser")
 	superkey  = flag.String("key", "c3R1cGlk", "base64 tsig key for superuser authentication")
 )
 
 func main() {
 	flag.Parse()
+	if *flagprof != "" {
+		f, err := os.Create(*flagprof)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
 	*superuser = strings.ToLower(*superuser)
 	conf := NewConfig()
 	conf.Rights[*superuser] = R_LIST | R_WRITE | R_DROP | R_USER // *all* of them
 
 	go func() {
-		conf.ServerUDP = &dns.Server{Addr: ":" + strconv.Itoa(*port) , Net: "udp", TsigSecret: map[string]string{dns.Fqdn(*superuser): *superkey}}
+		conf.ServerUDP = &dns.Server{Addr: ":" + strconv.Itoa(*port), Net: "udp", TsigSecret: map[string]string{dns.Fqdn(*superuser): *superkey}}
 		err := conf.ServerUDP.ListenAndServe()
 		if err != nil {
 			log.Fatal("fksd: could not start udp listener: %s", err.Error())
